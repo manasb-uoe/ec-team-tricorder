219cdc35dba28ba1734248d47922a4e743582539
Manas Bajaj <manas.bajaj94@gmail.com>
2015-02-24 09:02:44 +0000

    Changed the way requests are mapped to routes. Also deleted redundant route files.

--- tricorder_app/app.js
+++ tricorder_app/app.js
@@ -7,8 +7,7 @@ var bodyParser = require('body-parser');
 var swig = require('swig');
 var mongoose = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routesIndex = require('./routes/index');
 
 var app = express();
 
@@ -29,8 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes go here
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', routesIndex.home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
