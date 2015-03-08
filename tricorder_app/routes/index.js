7284fb001a434b484d9facb9bb5e89a7ae78d059
Manas Bajaj <manas.bajaj94@gmail.com>
2015-03-08 18:14:45 +0000

    Forgot to add nearby stops route in one of the previous commits. So, here it is.

--- tricorder_app/routes/index.js
+++ tricorder_app/routes/index.js
@@ -14,6 +14,42 @@ module.exports.home = function(req, res) {
     );
 };
 
+/* GET stops near me page */
+module.exports.nearbyStops = function (req, res) {
+    var currentUserLocation = {lat: req.query["lat"], lng: req.query["lng"]};
 
+    Stop.find(function (err, stops) {
+        if (!err) {
+            // find distance from user for each stop
+            for (var i=0; i<stops.length; i++) {
+                var stop = stops[i];
+                stop.distanceFromUser = util.getDistanceBetweenPoints(
+                    {lat: stop.coordinates[1], lng: stop.coordinates[0]},
+                    currentUserLocation
+                );
+            }
+
+            // sort all stops by distance from user
+            stops.sort(function (a, b) {
+                return a.distanceFromUser - b.distanceFromUser;
+            });
+
+            // humanize sorted results
+            var requestedStops = stops.slice(0, 10);
+            for (var i=0; i<requestedStops.length; i++) {
+                requestedStops[i].distanceFromUser = util.humanizeDistance(requestedStops[i].distanceFromUser);
+            }
+
+            // render page using sorted results
+            res.render('nearby_stops.html', {
+                    title: "Nearby Stops",
+                    current_url: globals.urls.nearby_stops,
+                    urls: globals.urls,
+                    stops: requestedStops
+                }
+            );
+        }
+    });
+};
 
 
