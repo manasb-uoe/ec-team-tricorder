c780f3b7caa627b2ddedf934623270d392677f38
Manas Bajaj <manas.bajaj94@gmail.com>
2015-03-08 18:08:05 +0000

    Updated util.js to add functions for computing and humanizing distance between two geographical coordinates.

--- tricorder_app/utilities/util.js
+++ tricorder_app/utilities/util.js
@@ -8,3 +8,32 @@ module.exports.raise404 = function(next) {
     err.message = "Not Found";
     next(err);
 };
+
+module.exports.getDistanceBetweenPoints = function(pt1, pt2) {
+    var rad = function (x) {
+        return x * Math.PI / 180;
+    };
+
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(pt2["lat"] - pt1["lat"]);
+    var dLong = rad(pt2["lng"] - pt1["lng"]);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(pt1["lat"])) * Math.cos(rad(pt2["lat"])) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+
+    return d; // returns the distance in meter
+};
+
+module.exports.humanizeDistance = function(d) {
+    var humanized = "";
+    if (d < 1000) {
+        humanized = d.toFixed(0) + " m";
+    } else {
+        humanized = (d/1000).toFixed(1) + " km";
+    }
+
+    return humanized;
+};
+
