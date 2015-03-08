c307e92e9340d8fb8bb1339c82d8835c87231c13
Manas Bajaj <manas.bajaj94@gmail.com>
2015-03-08 18:09:58 +0000

    Added home page template, stylesheet and route.

--- tricorder_app/routes/index.js
+++ tricorder_app/routes/index.js
@@ -1,6 +1,19 @@
+var globals = require('../utilities/globals');
+var util = require('../utilities/util');
+var async = require('async');
+var Stop = require('../models/stop').Stop;
+var Service = require('../models/service').Service;
 
 /* GET home page. */
 module.exports.home = function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('home.html', {
+            title: "Home",
+            current_url: globals.urls.home,
+            urls: globals.urls
+        }
+    );
 };
+
+
+
 
