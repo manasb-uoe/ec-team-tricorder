a89db452adf0e60c70e5ee45a8e4a3279624e92d
Manas Bajaj <manas.bajaj94@gmail.com>
2015-03-16 18:37:08 +0000

    Forgot to add stop_stats model in previous commit. So here it is.

---
+++ tricorder_app/models/stop_stats.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var StopStatsSchema = new mongoose.Schema({
+    date: String,
+    stop_id: Number,
+    early_5_plus: Number,
+    early_4: Number,
+    early_3: Number,
+    early_2: Number,
+    on_time: Number,
+    late_2: Number,
+    late_3: Number,
+    late_4: Number,
+    late_5_plus: Number,
+    total_count: Number,
+    modified: {type: Boolean, index: true}
+
+});
+
+//It is necessary to define the index at schema level for a compound index
+StopStatsSchema.index({date: 1, vehicle_id: 1});
+
+module.exports.StopStat = mongoose.model('StopStat', StopStatsSchema);
