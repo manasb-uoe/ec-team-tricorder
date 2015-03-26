3c26f10d7a1985c058f0e2eccb9b297dd452b25b
Manas Bajaj <manas.bajaj94@gmail.com>
2015-03-26 13:58:00 +0000

    Added tests for create, read and delete operations on all models.

---
+++ tricorder_app/tests/test.js
@@ -0,0 +1,372 @@
+/**
+ * Created by Manas on 25-03-2015.
+ */
+
+var request = require('supertest');
+var assert = require('assert');
+var mongoose = require('mongoose');
+var async = require('async');
+var dbConfig = require("../utilities/db");
+var testUtil = require("./util");
+
+// import models
+var Stop = require("../models/stop").Stop;
+var Service = require("../models/service").Service;
+var LiveLocation = require("../models/live_location").LiveLocation;
+var Timetable = require("../models/timetable").Timetable;
+var User = require("../models/user").User;
+var FavouriteStop = require("../models/favourite_stops").FavouriteStop;
+
+describe("test suite", function () {
+    before(function (done) {
+        mongoose.connect(dbConfig.test_db_url);
+        mongoose.connection.once('open', function() {
+            done();
+        });
+    });
+
+    describe("test create, read and delete operations on all models", function () {
+        describe("stop", function () {
+            before(function (done) {
+                testUtil.clearDB(mongoose.connection, done);
+            });
+
+            it("should add new stop successfully", function (done) {
+                var newStop = new Stop(testUtil.sampleStopOne);
+                newStop.save(function (err) {
+                    if (err) {throw err;}
+                    async.series(
+                        [
+                            function (callback) {
+                                // check if collection size is now 1
+                                Stop.count({}, function (err, count) {
+                                    assert.equal(count, 1);
+                                    callback(null);
+                                });
+                            },
+                            function (callback) {
+                                // verify saved data
+                                Stop.find({}, function (err, stops) {
+                                    if (err) {throw err;}
+                                    assert.equal(stops[0].stop_id, testUtil.sampleStopOne.stop_id);
+                                    assert.equal(stops[0].name, testUtil.sampleStopOne.name);
+                                    assert.equal(stops[0].identifier, testUtil.sampleStopOne.identifier);
+                                    assert.equal(stops[0].locality, testUtil.sampleStopOne.locality);
+                                    assert.equal(stops[0].orientation, testUtil.sampleStopOne.orientation);
+                                    assert.equal(stops[0].direction, testUtil.sampleStopOne.direction);
+                                    assert.equal(stops[0].coordinates.toString(), testUtil.sampleStopOne.coordinates.toString());
+                                    assert.equal(stops[0].destinations.toString(), testUtil.sampleStopOne.destinations.toString());
+                                    assert.equal(stops[0].services.toString(), testUtil.sampleStopOne.services.toString());
+                                    callback(null);
+                                });
+                            }
+                        ],
+                        function (err) {
+                            if (err) {throw err;}
+                            done();
+                        }
+                    );
+                });
+            });
+
+            it("should remove stop successfully", function (done) {
+                Stop.find({}, function (err, stops) {
+                    stops[0].remove(function (err) {
+                        if (err) {throw err;}
+                        Stop.count({}, function (err, count) {
+                            if (err) {throw err;}
+                            assert.equal(count, 0);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+
+        describe("service", function () {
+            before(function (done) {
+                testUtil.clearDB(mongoose.connection, done);
+            });
+
+            it("should add new service successfully", function (done) {
+                var newService = new Service(testUtil.sampleServiceOne);
+                newService.save(function (err) {
+                    if (err) {throw err;}
+                    async.series(
+                        [
+                            function (callback) {
+                                // check if collection size is now 1
+                                Service.count({}, function (err, count) {
+                                    assert.equal(count, 1);
+                                    callback(null);
+                                });
+                            },
+                            function (callback) {
+                                // verify saved data
+                                Service.find({}, function (err, services) {
+                                    if (err) {throw err;}
+                                    assert.equal(services[0].name, testUtil.sampleServiceOne.name);
+                                    assert.equal(services[0].description, testUtil.sampleServiceOne.description);
+                                    assert.equal(services[0].service_type, testUtil.sampleServiceOne.service_type);
+                                    assert.equal(services[0].routes.toString(), testUtil.sampleServiceOne.routes.toString());
+                                    callback(null);
+                                });
+                            }
+                        ],
+                        function (err) {
+                            if (err) {throw err;}
+                            done();
+                        }
+                    );
+                });
+            });
+
+            it("should remove service successfully", function (done) {
+                Service.find({}, function (err, services) {
+                    if (err) {throw err;}
+                    services[0].remove(function (err) {
+                        if (err) {throw err;}
+                        Service.count({}, function (err, count) {
+                            if (err) {throw err;}
+                            assert.equal(count, 0);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+
+        describe("live location", function () {
+            before(function (done) {
+                testUtil.clearDB(mongoose.connection, done);
+            });
+
+            it("should add new live location successfully", function (done) {
+                var newLiveLocation = new LiveLocation(testUtil.sampleLiveLocationOne);
+                newLiveLocation.save(function (err) {
+                    if (err) {throw err;}
+                    async.series(
+                        [
+                            function (callback) {
+                                // check if collection size is now 1
+                                LiveLocation.count({}, function (err, count) {
+                                    assert.equal(count, 1);
+                                    callback(null);
+                                });
+                            },
+                            function (callback) {
+                                // verify saved data
+                                LiveLocation.find({}, function (err, liveLocations) {
+                                    if (err) {throw err;}
+                                    assert.equal(liveLocations[0].vehicle_id, testUtil.sampleLiveLocationOne.vehicle_id);
+                                    assert.equal(liveLocations[0].last_gps_fix.toString(), testUtil.sampleLiveLocationOne.last_gps_fix.toString());
+                                    assert.equal(liveLocations[0].coordinates.toString(), testUtil.sampleLiveLocationOne.coordinates.toString());
+                                    assert.equal(liveLocations[0].service_name, testUtil.sampleLiveLocationOne.service_name);
+                                    assert.equal(liveLocations[0].destination, testUtil.sampleLiveLocationOne.destination);
+                                    callback(null);
+                                });
+                            }
+                        ],
+                        function (err) {
+                            if (err) {throw err;}
+                            done();
+                        }
+                    );
+                });
+            });
+
+            it("should remove live location successfully", function (done) {
+                LiveLocation.find({}, function (err, liveLocations) {
+                    if (err) {throw err;}
+                    liveLocations[0].remove(function (err) {
+                        if (err) {throw err;}
+                        LiveLocation.count({}, function (err, count) {
+                            if (err) {throw err;}
+                            assert.equal(count, 0);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+
+        describe("timetable", function () {
+            before(function (done) {
+                testUtil.clearDB(mongoose.connection, done);
+            });
+
+            it("should add new timetable successfully", function (done) {
+                var newTimetable = new Timetable(testUtil.sampleTimetableOne);
+                newTimetable.save(function (err) {
+                    if (err) {throw err;}
+                    async.series(
+                        [
+                            function (callback) {
+                                // check if collection size is now 1
+                                Timetable.count({}, function (err, count) {
+                                    assert.equal(count, 1);
+                                    callback(null);
+                                });
+                            },
+                            function (callback) {
+                                // verify saved data
+                                Timetable.find({}, function (err, timetables) {
+                                    if (err) {throw err;}
+                                    assert.equal(timetables[0].stop_id, testUtil.sampleTimetableOne.stop_id);
+                                    assert.equal(timetables[0].stop_name, testUtil.sampleTimetableOne.stop_name);
+                                    assert.equal(timetables[0].service_name, testUtil.sampleTimetableOne.service_name);
+                                    assert.equal(timetables[0].time, testUtil.sampleTimetableOne.time);
+                                    assert.equal(timetables[0].timestamp.toString(), testUtil.sampleTimetableOne.timestamp.toString());
+                                    assert.equal(timetables[0].destination, testUtil.sampleTimetableOne.destination);
+                                    assert.equal(timetables[0].day, testUtil.sampleTimetableOne.day);
+                                    callback(null);
+                                });
+                            }
+                        ],
+                        function (err) {
+                            if (err) {throw err;}
+                            done();
+                        }
+                    );
+                });
+            });
+
+            it("should remove timetable successfully", function (done) {
+                Timetable.find({}, function (err, timetables) {
+                    if (err) {throw err;}
+                    timetables[0].remove(function (err) {
+                        if (err) {throw err;}
+                        Timetable.count({}, function (err, count) {
+                            if (err) {throw err;}
+                            assert.equal(count, 0);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+
+        describe("user", function () {
+            before(function (done) {
+                testUtil.clearDB(mongoose.connection, done);
+            });
+
+            it("should add new user successfully", function (done) {
+                var newUser = new User(testUtil.sampleUserOne);
+                newUser.save(function (err) {
+                    if (err) {throw err;}
+                    async.series(
+                        [
+                            function (callback) {
+                                // check if collection size is now 1
+                                User.count({}, function (err, count) {
+                                    assert.equal(count, 1);
+                                    callback(null);
+                                });
+                            },
+                            function (callback) {
+                                // verify saved data
+                                User.find({}, function (err, users) {
+                                    if (err) {throw err;}
+                                    assert.equal(users[0].username, testUtil.sampleUserOne.username);
+                                    assert.equal(users[0].hash, testUtil.sampleUserOne.hash);
+                                    callback(null);
+                                });
+                            }
+                        ],
+                        function (err) {
+                            if (err) {throw err;}
+                            done();
+                        }
+                    );
+                });
+            });
+
+            it("should remove user successfully", function (done) {
+                User.find({}, function (err, users) {
+                    if (err) {throw err;}
+                    users[0].remove(function (err) {
+                        if (err) {throw err;}
+                        User.count({}, function (err, count) {
+                            if (err) {throw err;}
+                            assert.equal(count, 0);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+
+        describe("favourites stop", function () {
+            before(function (done) {
+                testUtil.clearDB(mongoose.connection, done);
+            });
+
+            it("should add new favourite stop successfully", function (done) {
+                async.waterfall(
+                    [
+                        function (callback) {
+                            var newUser = new User(testUtil.sampleUserOne);
+                            newUser.save(function (err, user) {
+                                if (err) {throw err;}
+                                callback(null, user);
+                            });
+                        },
+                        function (user, callback) {
+                            testUtil.sampleFavouriteStop.user_object_id = mongoose.Types.ObjectId(user._id);
+                            var newFavouriteStop = new FavouriteStop(testUtil.sampleFavouriteStop);
+                            newFavouriteStop.save(function (err) {
+                                if (err) {throw err;}
+                                async.series(
+                                    [
+                                        function (callback) {
+                                            // check if collection size is now 1
+                                            FavouriteStop.count({}, function (err, count) {
+                                                assert.equal(count, 1);
+                                                callback(null);
+                                            });
+                                        },
+                                        function (callback) {
+                                            // verify saved data
+                                            FavouriteStop.find({}, function (err, favouriteStops) {
+                                                if (err) {throw err;}
+                                                assert.equal(favouriteStops[0].user_object_id.toString(), testUtil.sampleFavouriteStop.user_object_id.toString());
+                                                assert.equal(favouriteStops[0].stop_id, testUtil.sampleFavouriteStop.stop_id);
+                                                assert.equal(favouriteStops[0].alt_name, testUtil.sampleFavouriteStop.alt_name);
+                                                callback(null);
+                                            });
+                                        }
+                                    ],
+                                    function (err) {
+                                        if (err) {throw err;}
+                                        callback(null);
+                                    }
+                                );
+                            });
+                        }
+                    ],
+                    function (err) {
+                        if (err) {throw err;}
+                        done();
+                    }
+                );
+            });
+
+            it("should remove favourite stop successfully", function (done) {
+                FavouriteStop.find(function (err, favouriteStops) {
+                    if (err) {throw err;}
+                    favouriteStops[0].remove(function (err) {
+                        if (err) {throw err;}
+                        FavouriteStop.count({}, function (err, count) {
+                            if (err) {throw err;}
+                            assert.equal(count, 0);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+
---
+++ tricorder_app/tests/util.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Manas on 26-03-2015.
+ */
+
+// import models
+var Stop = require("../models/stop").Stop;
+
+module.exports.clearDB = function (mongooseConnection, done) {
+    mongooseConnection.db.dropDatabase(function (err) {
+        if (err) {throw err;}
+        done();
+    });
+};
+
+module.exports.sampleStopOne = {
+    stop_id: 0,
+    name: "name_1",
+    identifier: "id_1",
+    locality: "locality_!",
+    orientation: 0,
+    direction: "direction_1",
+    coordinates : [0, 0],
+    destinations: [],
+    services: []
+};
+
+module.exports.sampleServiceOne = {
+    name: "name_1",
+    description: "destination_1",
+    service_type: "type_1",
+    routes: []
+};
+
+module.exports.sampleLiveLocationOne = {
+    vehicle_id: "id_1",
+    last_gps_fix: new Date(0),
+    coordinates: [0, 0],
+    service_name: "service_1",
+    destination: "destination_!"
+};
+
+module.exports.sampleTimetableOne = {
+    stop_id: "id_1",
+    stop_name: "name_1",
+    service_name: "service_name_1",
+    time: "0:00",
+    timestamp: new Date(0),
+    destination: "destination_1",
+    day: 0
+};
+
+module.exports.sampleUserOne = {
+    username: "username_1",
+    hash: "hash_!"
+};
+
+module.exports.sampleFavouriteStop = {
+    user_object_id: undefined,
+    stop_id: 0,
+    alt_name: "alt_name_1"
+};
