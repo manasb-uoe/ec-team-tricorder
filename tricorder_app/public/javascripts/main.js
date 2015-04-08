b4e03f8c50756b15a4affa6ca65cb952c1fb9fb1
Manas Bajaj <manas.bajaj94@gmail.com>
2015-04-08 22:13:05 +0100

    Added ability to display bus chart in a popup.

--- tricorder_app/public/javascripts/main.js
+++ tricorder_app/public/javascripts/main.js
@@ -490,7 +490,7 @@ var StopHandler = function () {
         var busLocations = {};
         $(".bus-container:visible").each(function () {
             var busContainer = $(this);
-            busLocations[busContainer.children(".title").text()] = {
+            busLocations[busContainer.find(".title").text()] = {
                 lat: busContainer.attr("data-lat"),
                 lng: busContainer.attr("data-lng")
             };
@@ -571,22 +571,22 @@ var FavouritesHandler = function () {
 var StatisticsChartHandler = function () {
     var config = {
         busChart: $("#bus-chart"),
-        stopChart: $("#stop-chart"),
+        //stopChart: $("#stop-chart"),
         busContainer: $(".bus-container"),
-        currentStopId: window.location.pathname.slice(-8)
+        currentStopId: window.location.pathname.slice(-8),
+        statsButton: $(".stats-button"),
+        busStatsModal: $("#bus-stats-modal")
     };
     function init() {
-        config.busChart.hide();
-        stopChartGenerator(config.currentStopId);
         bindUIActions();
     }
     function bindUIActions() {
-        config.busContainer.click(function () {
-            config.stopChart.hide();
-            var bus_id_chart_raw = $(this).find('.title').text();
+        config.statsButton.click(function (event) {
+            event.stopPropagation();
+            config.busStatsModal.modal();
+            var bus_id_chart_raw = $(this).parents(".bus-container").find('.title').text();
             var tmp = bus_id_chart_raw.substring(5, 8);
             var bus_id_chart = tmp.replace(' ', '');
-            config.busChart.show();
             busChartGenerator(bus_id_chart);
         });
     }
@@ -598,10 +598,11 @@ var StatisticsChartHandler = function () {
             success: function (json) {
                 config.busChart.highcharts({
                     chart: {
-                        type: 'column'
+                        type: 'column',
+                        width: 868
                     },
                     title: {
-                        text: 'Bus statistics for bus ' + bus_id_chart
+                        text: 'Bus statistics for bus #' + bus_id_chart
                     },
                     xAxis: {
                         type: 'category',
@@ -666,82 +667,82 @@ var StatisticsChartHandler = function () {
             }
         });
     }
-    function stopChartGenerator(currentStop) {
-        $.ajax({
-            url: "/api/stop/" + "?id=" + currentStop + "&period=" + "daily",
-            type: "GET",
-            dataType: "json",
-            success: function (json) {
-                config.stopChart.highcharts({
-                    chart: {
-                        type: 'column'
-                    },
-                    title: {
-                        text: 'Statistics for bus stop ' + currentStop
-                    },
-                    xAxis: {
-                        type: 'category',
-                        labels: {
-                            rotation: -45,
-                            style: {
-                                fontSize: '13px',
-                                fontFamily: 'Verdana, sans-serif'
-                            }
-                        }
-                    },
-                    yAxis: {
-                        min: 0,
-                        title: {
-                            text: 'Number of buses'
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    tooltip: {
-                        pointFormat: 'Number of buses: <b>{point.y:.1f}</b>'
-                    },
-                    series: [{
-                        name: 'Population',
-                        data: [
-                            ['Early by more than 10 min', json["early_10_plus"]],
-                            ['Early by 10 min', json["early_10"]],
-                            ['Early by 9 min', json["early_9"]],
-                            ['Early by 8 min', json["early_8"]],
-                            ['Early by 7 min', json["early_7"]],
-                            ['Early by 6 min', json["early_6"]],
-                            ['Early by 4 min', json["early_4"]],
-                            ['Early by 3 min', json["early_3"]],
-                            ['Early by 2 min', json["early_2"]],
-                            ['On time', json["on_time"]],
-                            ['Late by 2 min', json["late_2"]],
-                            ['Late by 3 min', json["late_3"]],
-                            ['Late by 4 min', json["late_4"]],
-                            ['Late by 5 min', json["late_5"]],
-                            ['Late by 6 min', json["late_6"]],
-                            ['Late by 7 min', json["late_7"]],
-                            ['Late by 8 min', json["late_8"]],
-                            ['Late by 9 min', json["late_9"]],
-                            ['Late by 10 min', json["late_10"]],
-                            ['Late by more than', json["late_10_plus"]]
-                        ],
-                        dataLabels: {
-                            enabled: true,
-                            rotation: -90,
-                            color: '#FFFFFF',
-                            align: 'right',
-                            format: '{point.y:.1f}',
-                            y: 10,
-                            style: {
-                                fontSize: '13px',
-                                fontFamily: 'Verdana, sans-serif'
-                            }
-                        }
-                    }]
-                });
-            }
-        });
-    }
+    //function stopChartGenerator(currentStop){
+    //    $.ajax({
+    //        url: "/api/stop/" + "?id=" + currentStop + "&period=" + "daily",
+    //        type: "GET",
+    //        dataType: "json",
+    //        success: function (json) {
+    //            config.stopChart.highcharts({
+    //                chart: {
+    //                    type: 'column'
+    //                },
+    //                title: {
+    //                    text: 'Statistics for bus stop '+ currentStop
+    //                },
+    //                xAxis: {
+    //                    type: 'category',
+    //                    labels: {
+    //                        rotation: -45,
+    //                        style: {
+    //                            fontSize: '13px',
+    //                            fontFamily: 'Verdana, sans-serif'
+    //                        }
+    //                    }
+    //                },
+    //                yAxis: {
+    //                    min: 0,
+    //                    title: {
+    //                        text: 'Number of buses'
+    //                    }
+    //                },
+    //                legend: {
+    //                    enabled: false
+    //                },
+    //                tooltip: {
+    //                    pointFormat: 'Number of buses: <b>{point.y:.1f}</b>'
+    //                },
+    //                series: [{
+    //                    name: 'Population',
+    //                    data: [
+    //                        ['Early by more than 10 min', json["early_10_plus"]],
+    //                        ['Early by 10 min', json["early_10"]],
+    //                        ['Early by 9 min', json["early_9"]],
+    //                        ['Early by 8 min', json["early_8"]],
+    //                        ['Early by 7 min', json["early_7"]],
+    //                        ['Early by 6 min', json["early_6"]],
+    //                        ['Early by 4 min', json["early_4"]],
+    //                        ['Early by 3 min', json["early_3"]],
+    //                        ['Early by 2 min', json["early_2"]],
+    //                        ['On time', json["on_time"]],
+    //                        ['Late by 2 min', json["late_2"]],
+    //                        ['Late by 3 min', json["late_3"]],
+    //                        ['Late by 4 min', json["late_4"]],
+    //                        ['Late by 5 min', json["late_5"]],
+    //                        ['Late by 6 min', json["late_6"]],
+    //                        ['Late by 7 min', json["late_7"]],
+    //                        ['Late by 8 min', json["late_8"]],
+    //                        ['Late by 9 min', json["late_9"]],
+    //                        ['Late by 10 min', json["late_10"]],
+    //                        ['Late by more than', json["late_10_plus"]]
+    //                    ],
+    //                    dataLabels: {
+    //                        enabled: true,
+    //                        rotation: -90,
+    //                        color: '#FFFFFF',
+    //                        align: 'right',
+    //                        format: '{point.y:.1f}', // one decimal
+    //                        y: 10, // 10 pixels down from the top
+    //                        style: {
+    //                            fontSize: '13px',
+    //                            fontFamily: 'Verdana, sans-serif'
+    //                        }
+    //                    }
+    //                }]
+    //            });
+    //        }
+    //    });
+    //}
     return {
         init: init
     };
